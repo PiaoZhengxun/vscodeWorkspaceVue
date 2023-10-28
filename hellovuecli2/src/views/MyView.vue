1f09448a9b76b439013a940ed486211cc65a56f1
PiaoZhengxun <pjh951218@naver.com>
2023-10-29 00:03:17 +0800

    2023.10.09

---
+++ hellovuecli2/src/views/MyView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+     <img alt="Vue logo" src="../assets/logo.png">
+    <h1>This is an MyView page</h1>
+    <mycomponent></mycomponent>
+  </div>
+</template>
+<script>
+import mycomponent from '@/components/MyComponent.vue'
+
+
+
+export default {
+        
+ components:{
+   mycomponent
+ }
+
+
+}
+</script>
---
+++ hellovuecli2/vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    open: true,
+    host: '0.0.0.0',
+    port: 8081,
+  }
+})
